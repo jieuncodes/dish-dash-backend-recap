76f3057f71e643a40a4136ba05b5c8a64926b813
jieuncodes <youredith3@gmail.com>
2024-01-07 20:48:35 +0900

    feat: add class-validator to entity

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,4 +1,5 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @InputType({ isAbstract: true })
@@ -11,21 +12,16 @@ export class Restaurant {
 
   @Field(() => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
   @Field(() => Boolean)
   @Column()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(() => String)
   @Column()
   address: string;
-
-  @Field(() => String)
-  @Column()
-  ownerName: string;
-
-  @Field(() => String)
-  @Column()
-  categoryName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -6,10 +6,12 @@ import { RestaurantService } from './restaurants.service';
 @Resolver(() => Restaurant)
 export class RestaurantResolver {
   constructor(private readonly restaurantService: RestaurantService) {}
+
   @Query(() => [Restaurant])
   restaurants(): Promise<Restaurant[]> {
     return this.restaurantService.getAll();
   }
+
   @Mutation(() => Boolean)
   async createRestaurant(
     @Args('input') createRestaurantDto: CreateRestaurantDto,
