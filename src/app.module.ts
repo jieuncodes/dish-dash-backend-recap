90a0734fbe77c37580e5d57dd90f1a9754c1a0c4
jieuncodes <youredith3@gmail.com>
2024-01-07 13:59:34 +0900

    feat: Add TypeORM decorators to Restaurant entity
    - Added @Column decorators to define database columns
    - Added @Entity decorator to reflect new relationships
    - Checked column synchronizing by adding new column on Postico.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 
 @Module({
   imports: [
@@ -34,8 +35,9 @@ import * as Joi from 'joi';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
+      entities: [Restaurant],
     }),
     RestaurantsModule,
   ],
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,18 +1,32 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { RestaurantsResolver } from '../restaurants.resolver';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(() => Number)
+  id: number;
+
   @Field(() => String)
+  @Column()
   name: string;
 
   @Field(() => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field(() => String)
+  @Column()
   address: string;
 
   @Field(() => String)
+  @Column()
   ownerName: string;
+
+  @Field(() => String)
+  @Column()
+  categoryName: string;
 }
 providers: [RestaurantsResolver];
