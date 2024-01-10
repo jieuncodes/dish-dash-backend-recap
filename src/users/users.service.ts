fefc706560bebc25423bc6bb16dc6226f37844ec
jieuncodes <youredith3@gmail.com>
2024-01-10 20:50:22 +0900

    feat: findOne user by email and save

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,3 +1,4 @@
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -8,4 +9,17 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({ email, password, role }: CreateAccountInput) {
+    try {
+      const exists = await this.users.findOne({ where: { email } });
+      if (exists) {
+        return 'There is a user with that email already.';
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+      return true;
+    } catch (e) {
+      return "Couldn't create account for some reason.";
+    }
+  }
 }
