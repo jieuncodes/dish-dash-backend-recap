7b55f2ee715eef9df462077081251714811da809
mars <youredith3@gmail.com>
2024-01-16 20:33:58 +0900

    feat: create JWT service and turn JwtModule into a dynamic module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,12 +6,14 @@ import { User } from './entities/user.entity';
 import { LoginInput } from './dtos/login.dto';
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
     private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async createAccount({
