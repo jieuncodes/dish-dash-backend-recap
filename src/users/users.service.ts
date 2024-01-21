6be6a751701c0b4d6652add84c1e124e52d5b8ad
jieuncodes <youredith3@gmail.com>
2024-01-21 20:42:25 +0900

    feat: save verification entity when create account

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,20 @@
-import { CreateAccountInput } from './dtos/create-account.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
-import { LoginInput } from './dtos/login.dto';
 import { JwtService } from 'src/jwt/jwt.service';
+import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { EditProfileInput } from './dtos/edit-profile.dto';
+import { LoginInput } from './dtos/login.dto';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
     private readonly jwtService: JwtService,
+    @InjectRepository(Verification)
+    private readonly verification: Repository<Verification>,
   ) {}
 
   async createAccount({
@@ -26,7 +29,15 @@ export class UsersService {
         return { ok: false, error: 'There is a user with that email already.' };
       }
 
-      await this.users.save(this.users.create({ email, password, role }));
+      const user = await this.users.save(
+        this.users.create({ email, password, role }),
+      );
+      await this.verification.save(
+        this.verification.create({
+          user,
+        }),
+      );
+
       return { ok: true };
     } catch (e) {
       return { ok: false, error: "Couldn't create account" };
