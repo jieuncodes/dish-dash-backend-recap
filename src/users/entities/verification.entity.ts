089c066d12dff276ded50070f0cc0d9ce4201717
jieuncodes <youredith3@gmail.com>
2024-01-23 19:48:53 +0900

    chore: install uuid

--- src/users/entities/verification.entity.ts
+++ src/users/entities/verification.entity.ts
@@ -1,7 +1,7 @@
-import { v4 as uuidv4 } from 'uuid';
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { BeforeInsert, Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
 import { User } from './user.entity';
 
 @InputType({ isAbstract: true })
