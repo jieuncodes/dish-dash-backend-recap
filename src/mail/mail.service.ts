27a4ac311b5d0962548fc2d7d7a7d56eb4a92dfd
mars <youredith3@gmail.com>
2024-01-28 18:49:41 +0900

    feat: mailgun email template

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -12,12 +12,14 @@ export class MailService {
     this.sendEmail('testing', 'test');
   }
 
-  private async sendEmail(subject: string, content: string) {
+  private async sendEmail(subject: string, template: string) {
     const form = new FormData();
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', `youredith@gmail.com`);
     form.append('subject', subject);
-    form.append('text', content);
+    form.append('template', template);
+    form.append('v:code', 'asdfasdf');
+    form.append('v:username', 'Jieun');
 
     const response = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
