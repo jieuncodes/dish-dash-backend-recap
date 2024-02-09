698b735385becda60619b5faa03e5ccbdd76d034
mars <youredith3@gmail.com>
2024-02-09 18:56:47 +0900

    feat: create MockRepository type using generic
    unit test: to see each line of code is doing what we told the code to do

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -5,6 +5,7 @@ import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
 
 const mockRepository = {
   findOne: jest.fn(),
@@ -19,8 +20,11 @@ const mockMailService = {
   sendVerificationEmail: jest.fn(),
 };
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('UserService', () => {
   let service: UsersService;
+  let usersRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -45,15 +49,19 @@ describe('UserService', () => {
       ],
     }).compile();
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-
-  it.todo('createAccount');
-  it.todo('login');
-  it.todo('findById');
-  it.todo('editProfile');
-  it.todo('verifyEmail');
 });
+
+describe('createAccount', () => {
+  it('should fail if user exists', () => {});
+});
+
+it.todo('login');
+it.todo('findById');
+it.todo('editProfile');
+it.todo('verifyEmail');
