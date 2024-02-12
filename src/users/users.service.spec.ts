1265998b2fd5a4a554a816dc977bfb9da01a5f52
mars <youredith3@gmail.com>
2024-02-12 09:02:48 +0900

    test: exception test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -111,6 +111,12 @@ describe('UserService', () => {
       );
       expect(result).toEqual({ ok: true });
     });
+    it('should fail on exception', async () => {
+      usersRepository.findOne.mockRejectedValue(new Error());
+
+      const result = await service.createAccount(createAccountArgs);
+      expect(result).toEqual({ ok: false, error: "Couldn't create account" });
+    });
   });
 
   it.todo('login');
