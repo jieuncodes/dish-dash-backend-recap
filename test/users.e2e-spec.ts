28875a27e193888dd8830e41e635804cef8762d8
mars <youredith3@gmail.com>
2024-02-14 06:42:35 +0900

    test: setup user module e2e test
    test resolvers - e2e

---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+
+describe('UserModule (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+});
