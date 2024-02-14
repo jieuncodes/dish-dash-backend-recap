1639a9cf7b1e7585b24e1eee9849d5718bc0927c
mars <youredith3@gmail.com>
2024-02-14 20:28:14 +0900

    test: createAccount returns

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -51,7 +51,11 @@ describe('UserModule (e2e)', () => {
   }
 }`,
         })
-        .expect(200);
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
     });
   });
   it.todo('userProfile');
