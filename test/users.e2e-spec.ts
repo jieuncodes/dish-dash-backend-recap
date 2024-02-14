030b0c9992cbf4489ccf7d959355ecd3bef8b4f6
mars <youredith3@gmail.com>
2024-02-14 20:24:16 +0900

    test: post createAccount e2e
    create user table for test -> then clean up the table

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,7 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
-import { DataSource, getConnection } from 'typeorm';
+import { DataSource } from 'typeorm';
+import * as request from 'supertest';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -30,7 +33,27 @@ describe('UserModule (e2e)', () => {
     await app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'jieun@codes.com';
+
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation{
+  createAccount(input: {
+    email: "${EMAIL}",
+    password:"12345", 
+    role:Owner
+  }){
+    ok
+    error
+  }
+}`,
+        })
+        .expect(200);
+    });
+  });
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
